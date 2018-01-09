8db9835a106d66b68d18f02bfef5ba4a03c90e10
juliocarneiro <juliocarneiro@gmail.com>
2018-01-09 15:37:35 -0200

    home

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,3 @@
-//LOADER
-$('html').append('<body><div id="space-loader"></div><div id="loader"></div></body>');
-$(window).load(function(){
-	$('#loader, #space-loader').fadeOut();
-	$('body').fadeIn(500);
-});
+(function(){
+	
+})();
